cdce520730194943a0c207a9ebbbe2d83790c258
dev-italosantos <italotayllor@gmail.com>
2022-03-05 21:23:03 -0300

    feat: declarar calsses

---
+++ src/anatomy-classes.js
@@ -0,0 +1,6 @@
+class Retangulo {
+    constructor(altura, largura) {
+        this.altura = altura;
+        this.largura = largura;
+    }
+}
--- src/anatomy-function.js
+++ src/anatomy-function.js
@@ -14,21 +14,18 @@ myFunction(myCarro)
 
 console.log(myCarro.marca)
 
-// Function Constructor
 var multiply = new Function('x', 'y', 'return x * y')
 
 console.log(multiply(7, 5))
 
-// Function Anônima
 var play = function(x, y) {
     return x * y
 }
 
 console.log(play(10, 7))
 
-// Function expression de uma função nomeada
 var xbox = function func_name(x, y) {
     return x * y
 }
 
-console.log(xbox(5,5));
+console.log(xbox(5,5))
