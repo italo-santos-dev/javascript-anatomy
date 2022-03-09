9370fc4bb15d61c1a1e7ba6a92b7fe49088a38fb
dev-italosantos <italotayllor@gmail.com>
2022-03-09 00:41:59 -0300

    feat: Adicionar filter por id

--- src/anatomy-filter-3.js
+++ src/anatomy-filter-3.js
@@ -1,15 +1,21 @@
 let asp = [
     {_id: 'eyJhbGciOiJIUzI1NiJ9'},
+    {_id: NaN},
     {_id: 'eyJhbGciOiJIUzI1NiJ8'},
+    {_id: null },
     {_id: 'eyJhbGciOiJIUzI1NiJ7'},
+    {_id: 'undefined' },
     {_id: 'eyJhbGciOiJIUzI1NiJ6'},
-    {_id: 5}
+    {_id: 5},
+    {},
+    {_id: 6},
+    'foo', 'bar', 'foo', 
 ]
 
 let campoAsp = []
 
 for (var i = 0; i < asp.length; i++) {
-    if (asp[i]._id !== null) {
+    if (asp[i]._id != null) {
         campoAsp.push(asp[i])
     }
 }
@@ -22,3 +28,22 @@ var aspCampoNumero = asp.filter(function(asP) {
 })
 
 console.log(aspCampoNumero)
+
+
+var aspNoDuplice = asp.filter(function(asP, i) {
+    return asp.indexOf(asP) === i
+})
+
+console.log(aspNoDuplice)
+
+var aspCampoNoDuplice = asp.filter((este, i) => asp.indexOf(este) === i)
+
+console.log(aspCampoNoDuplice)
+
+var aspCampoNuNull = asp.filter(function isNaN(asP) {
+    return asP._id
+})
+
+console.log(aspCampoNuNull)
+
+
