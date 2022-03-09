93984ae17dea8e23788129355ab269bb574b14e1
dev-italosantos <italotayllor@gmail.com>
2022-03-08 23:42:56 -0300

    feat: Adicionar filter por id

--- src/anatomy-filter-3.js
+++ src/anatomy-filter-3.js
@@ -2,7 +2,8 @@ let asp = [
     {_id: 'eyJhbGciOiJIUzI1NiJ9'},
     {_id: 'eyJhbGciOiJIUzI1NiJ8'},
     {_id: 'eyJhbGciOiJIUzI1NiJ7'},
-    {_id: 'eyJhbGciOiJIUzI1NiJ6'}
+    {_id: 'eyJhbGciOiJIUzI1NiJ6'},
+    {_id: 5}
 ]
 
 let campoAsp = []
@@ -14,3 +15,10 @@ for (var i = 0; i < asp.length; i++) {
 }
 
 console.log(campoAsp)
+
+
+var aspCampoNumero = asp.filter(function(asP) {
+    return typeof(asP._id) === 'number'
+})
+
+console.log(aspCampoNumero)
