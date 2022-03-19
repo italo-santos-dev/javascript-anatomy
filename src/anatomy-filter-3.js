db7b14f8a36e6f179bbd62d1f5de07ee72b7dd33
dev-italosantos <italotayllor@gmail.com>
2022-03-18 22:42:13 -0300

    refactor: Refatoração anatomy-filter

--- src/anatomy-filter-3.js
+++ src/anatomy-filter-3.js
@@ -14,36 +14,36 @@ let asp = [
 
 let campoAsp = []
 
-for (var i = 0; i < asp.length; i++) {
-    if (asp[i]._id === 'eyJhbGciOiJIUzI1NiJ9') {
-        campoAsp.push(asp[i])
-    }
-}
+// for (var i = 0; i < asp.length; i++) {
+//     if (asp[i]._id === 'eyJhbGciOiJIUzI1NiJ9') {
+//         campoAsp.push(asp[i])
+//     }
+// }
 
-console.log(campoAsp)
-
-
-var aspCampoNumero = asp.filter(function(asP) {
-    return typeof(asP._id) === 'number'
-})
-
-console.log(aspCampoNumero)
+// console.log(campoAsp)
 
 
-var aspNoDuplice = asp.filter(function(asP, i) {
-    return asp.indexOf(asP) === i
-})
+// var aspCampoNumero = asp.filter(function(asP) {
+//     return typeof(asP._id) === 'number'
+// })
 
-console.log(aspNoDuplice)
+// console.log(aspCampoNumero)
+
+
+// var aspNoDuplice = asp.filter(function(asP, i) {
+//     return asp.indexOf(asP) === i
+// })
+
+// console.log(aspNoDuplice)
 
 var aspCampoNoDuplice = asp.filter((este, i) => asp.indexOf(este) === i)
 
 console.log(aspCampoNoDuplice)
 
-var aspCampoNuNull = asp.filter(function isNaN(asP) {
-    return asP._id
-})
+// var aspCampoNuNull = asp.filter(function isNaN(asP) {
+//     return asP._id
+// })
 
-console.log(aspCampoNuNull)
+// console.log(aspCampoNuNull)
 
 
