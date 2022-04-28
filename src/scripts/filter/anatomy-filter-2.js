1e90fc741df17bc28a01764161e4befe00e803fd
dev-italosantos <italotayllor@gmail.com>
2022-04-27 22:07:51 -0300

    refactor: Cut default javascript to code standar anatomy-filter-2

--- src/scripts/filter/anatomy-filter-2.js
+++ src/scripts/filter/anatomy-filter-2.js
@@ -1,22 +1,22 @@
-var produtos = [
-    {id: 1, descricao: "Smartphone", categoria: "Eletrônico"},
-    {id: 2, descricao: "Notebook", categoria: "Eletrônico"},
-    {id: 3, descricao: "Geladeira", categoria: "Eletrodoméstico"},
-    {},
-    {id: null },
-    {id: NaN},
-    {id: 'undefined' },
-    {id: 4, descricao: "Liquidificador", categoria: "Eletrodoméstico"},
-    {id: 5, descricao: "Fogão", categoria: "Eletrodoméstico"}
+const produtos = [
+  { id: 1, descricao: 'Smartphone', categoria: 'Eletrônico' },
+  { id: 2, descricao: 'Notebook', categoria: 'Eletrônico' },
+  { id: 3, descricao: 'Geladeira', categoria: 'Eletrodoméstico' },
+  {},
+  { id: null },
+  { id: NaN },
+  { id: 'undefined' },
+  { id: 4, descricao: 'Liquidificador', categoria: 'Eletrodoméstico' },
+  { id: 5, descricao: 'Fogão', categoria: 'Eletrodoméstico' }
 ]
 
-function filtrarPorDescricao(value) {
-    if ('id' in value && typeof(value.id) === 'number' && !isNaN(value.id)) {
-         return value;
-    } 
+function filtrarPorDescricao (value) {
+  if ('id' in value && typeof (value.id) === 'number' && !isNaN(value.id)) {
+    return value
+  }
 }
 
-var produtosValidos = produtos.filter(filtrarPorDescricao);
-produtosValidos.forEach(produto => { 
-    console.log(produto);
+const produtosValidos = produtos.filter(filtrarPorDescricao)
+produtosValidos.forEach(produto => {
+  console.log(produto)
 })
