f69ef5e47764f3e2ce9185ccb82b2a1a0949dbe7
dev-italosantos <italotayllor@gmail.com>
2022-04-27 22:08:27 -0300

    refactor: Cut default javascript to code standar

--- src/scripts/anatomy-1.js
+++ src/scripts/anatomy-1.js
@@ -1,31 +1,31 @@
 // function decorators
 
-function direSalut() {
-    console.log('Salut')
+function direSalut () {
+  console.log('Salut')
 }
 
 direSalut()
 
-function sayHello(name) {
-    console.log('Hello ' + name)
+function sayHello (name) {
+  console.log('Hello ' + name)
 }
 
 sayHello('Italo Santos')
 
-function returnHi() {
-    return 'Hi'
+function returnHi () {
+  return 'Hi'
 }
 
 const greeting = returnHi()
 console.log(greeting)
 
-function returnHiTo(name) {
-    return `Hi ${name}`
+function returnHiTo (name) {
+  return `Hi ${name}`
 }
 
 console.log(returnHiTo('Andrew'))
 
-const decoder = new TextDecoder("utf-8");
-const str = decoder.decode(Buffer.from([0xAA, 0xAA]));
+const decoder = new TextDecoder('utf-8')
+const str = decoder.decode(Buffer.from([0xAA, 0xAA]))
 
 console.log(str)
