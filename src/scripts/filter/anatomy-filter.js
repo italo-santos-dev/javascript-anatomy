f380fb362615a925cd5ac939746db20fed5038c9
dev-italosantos <italotayllor@gmail.com>
2022-04-27 22:08:03 -0300

    refactor: Cut default javascript to code standar anatomy-filter

--- src/scripts/filter/anatomy-filter.js
+++ src/scripts/filter/anatomy-filter.js
@@ -1,15 +1,14 @@
-var produtos = [
-    {_id: 1, descricao: "Smartphone", categoria: "Eletrônico"},
-    {_id: 2, descricao: "Notebook", categoria: "Eletrônico"},
-    {_id: 3, descricao: "Geladeira", categoria: "Eletrodoméstico"},
-    {_id: 4, descricao: "Liquidificador", categoria: "Eletrodoméstico"},
-    {_id: 5, descricao: "Fogão", categoria: "Eletrodoméstico"}
+const produtos = [
+  { _id: 1, descricao: 'Smartphone', categoria: 'Eletrônico' },
+  { _id: 2, descricao: 'Notebook', categoria: 'Eletrônico' },
+  { _id: 3, descricao: 'Geladeira', categoria: 'Eletrodoméstico' },
+  { _id: 4, descricao: 'Liquidificador', categoria: 'Eletrodoméstico' },
+  { _id: 5, descricao: 'Fogão', categoria: 'Eletrodoméstico' }
 ]
-function retornaEletronico (value){
-    if (value._id !== null)
-    return value;
+function retornaEletronico (value) {
+  if (value._id !== null) { return value }
 }
-var produtosEletronico = produtos.filter(retornaEletronico);
-produtosEletronico.forEach(produtoEletro => { 
-    console.log(produtoEletro);
+const produtosEletronico = produtos.filter(retornaEletronico)
+produtosEletronico.forEach(produtoEletro => {
+  console.log(produtoEletro)
 })
