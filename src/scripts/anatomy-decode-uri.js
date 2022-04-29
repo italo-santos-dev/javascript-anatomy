d91e3c95463405905b059ff59a51e52cf46c6236
dev-italosantos <italotayllor@gmail.com>
2022-04-28 23:23:16 -0300

    refactor: Cut default javascript to code standar decode-uri

--- src/scripts/anatomy-decode-uri.js
+++
@@ -1,9 +0,0 @@
-const uri = 'https://mozilla.org/?x=шеллы'
-const encoded = encodeURI(uri)
-console.log(encoded)
-
-try {
-    console.log(decodeURI(encoded))
-} catch (err) {
-    console.error(err)
-}
