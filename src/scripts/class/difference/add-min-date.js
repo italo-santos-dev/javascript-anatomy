9776c7fa59b2bbdd70e79a8fc5b6f9c7017b9d1c
dev-italosantos <italotayllor@gmail.com>
2022-05-07 22:10:55 -0300

    feat: add days, hours and minutes

--- src/scripts/class/difference/add-min-date.js
+++ src/scripts/class/difference/add-min-date.js
@@ -1,5 +1,12 @@
 let data = new Date();
 
+const onehour = 1000*60*60;
+const oneDya = 1000*60*60*24;
+
 data.setMinutes(data.getMinutes() + 3);
 
-console.log(data)
+console.log(new Date((new Date()).getTime() + oneDya));
+
+data.setHours(data.getHours() + 1);
+
+console.log(data);
