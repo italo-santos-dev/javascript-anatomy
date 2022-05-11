ed589a6367a02bf7f6db408f4cc64037d2865bcc
dev-italosantos <italotayllor@gmail.com>
2022-05-11 18:26:39 -0300

    refactor: Cut default javascript to code standar anatomy filter 3

--- src/scripts/filter/anatomy-filter-3.js
+++ src/scripts/filter/anatomy-filter-3.js
@@ -1,48 +1,46 @@
-let asp = [
-    { _id: 'eyJhbGciOiJIUzI1NiJ9' },
-    { _id: NaN },
-    { _id: 'eyJhbGciOiJIUzI1NiJ8' },
-    { _id: null },
-    { _id: 'eyJhbGciOiJIUzI1NiJ7' },
-    { _id: 'undefined' },
-    { _id: 'eyJhbGciOiJIUzI1NiJ6' },
-    { _id: 5 },
-    {},
-    { _id: 6 },
-    {},
-    'foo', 'bar', 'foo',
+const asp = [
+  { id: 'eyJhbGciOiJIUzI1NiJ9' },
+  { _id: NaN },
+  { _id: 'eyJhbGciOiJIUzI1NiJ8' },
+  { _id: null },
+  { _id: 'eyJhbGciOiJIUzI1NiJ7' },
+  { _id: 'undefined' },
+  { _id: 'eyJhbGciOiJIUzI1NiJ6' },
+  { _id: 5 },
+  {},
+  { _id: 6 },
+  {},
+  'foo', 'bar', 'foo'
 ]
 
-let campoAsp = []
+const campoAsp = []
 
-for (var i = 0; i < asp.length; i++) {
-    if (asp[i]._id === 'eyJhbGciOiJIUzI1NiJ9') {
-        campoAsp.push(asp[i])
-    }
+for (let i = 0; i < asp.length; i++) {
+  if (asp[i]._id === 'eyJhbGciOiJIUzI1NiJ9') {
+    campoAsp.push(asp[i])
+  }
 }
 
 console.log(campoAsp)
 
-
-var aspCampoNumero = asp.filter(function (asP) {
-    return typeof (asP._id) === 'number'
+const aspCampoNumero = asp.filter(function (asP) {
+  return typeof (asP._id) === 'number'
 })
 
 console.log(aspCampoNumero)
 
-
-var aspNoDuplice = asp.filter(function (asP, i) {
-    return asp.indexOf(asP) === i
+const aspNoDuplice = asp.filter(function (asP, i) {
+  return asp.indexOf(asP) === i
 })
 
 console.log(aspNoDuplice)
 
-var aspCampoNoDuplice = asp.filter((este, i) => asp.indexOf(este) === i)
+const aspCampoNoDuplice = asp.filter((este, i) => asp.indexOf(este) === i)
 
 console.log(aspCampoNoDuplice)
 
-var aspCampoNuNull = asp.filter(function isNaN(asP) {
-    return asP._id
+const aspCampoNuNull = asp.filter(function isNaN (asP) {
+  return asP._id
 })
 
 console.log(aspCampoNuNull)
