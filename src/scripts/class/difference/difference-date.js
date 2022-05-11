93e93ea22f566c1e77b3f28dbf2daf98d439dc66
dev-italosantos <italotayllor@gmail.com>
2022-05-11 18:25:02 -0300

    refactor: Cut default javascript to code standar difference-date

--- src/scripts/class/difference/difference-date.js
+++ src/scripts/class/difference/difference-date.js
@@ -3,3 +3,16 @@ let date2 = new Date("2022-04-10");
 let timeDiff = Math.abs(date2.getTime() - date1.getTime());
 let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 console.log(diffDays)
+
+let gg = new Date();
+let el = new Date();
+
+if (gg.toDateString() === el.toDateString()) {
+    console.log(el)   
+}
+
+let dateGg = new Date();
+
+let dateEl = new Date("2022-05-05T20:34:34.073Z")
+
+console.log(dateGg.valueOf() == dateEl.valueOf())
