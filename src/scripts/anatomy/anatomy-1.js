4da10021add63d1a82c8237799990ecc2761d02b
dev-italosantos <italotayllor@gmail.com>
2022-05-18 22:03:45 -0300

    feat: add folder anatomy

---
+++ src/scripts/anatomy/anatomy-1.js
@@ -0,0 +1,31 @@
+// function decorators
+
+function direSalut () {
+  console.log('Salut')
+}
+
+direSalut()
+
+function sayHello (name) {
+  console.log('Hello ' + name)
+}
+
+sayHello('Italo Santos')
+
+function returnHi () {
+  return 'Hi'
+}
+
+const greeting = returnHi()
+console.log(greeting)
+
+function returnHiTo (name) {
+  return `Hi ${name}`
+}
+
+console.log(returnHiTo('Andrew'))
+
+const decoder = new TextDecoder('utf-8')
+const str = decoder.decode(Buffer.from([0xAA, 0xAA]))
+
+console.log(str)
