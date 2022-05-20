a1a71f52edfa6a7782a2436021296a29fa53d946
dev-italosantos <italotayllor@gmail.com>
2022-05-19 22:38:14 -0300

    feat: add function for add setTime hours 6

--- src/scripts/convert/convert-data.js
+++ src/scripts/convert/convert-data.js
@@ -11,20 +11,14 @@ function converteDate(str) {
 
 console.log(converteDate("15/03/22"))
 
-
-//03/03/2031
-//15/03/2030
-
 // d / m / yyyy
 let object = "03/15/2030"
 
 // m / d / yyyy
-// let err = "05/15/2030"
-
-// 2023-03-03
+let err = "05/15/2030"
 
 let data = object.split('/');
-// let dataErr = err.split("/");
+let dataErr = err.split("/");
 
 let dataValidacaoDoRegistro = new Date(data[2], data[0] - 1, data[1]);
 
@@ -34,10 +28,25 @@ console.log(dataValidacaoDoRegistro)
 
 //  let dataValidErr = new Date(Date.UTC(dataErr[2],  dataErr[0], dataErr[1]));
 
-//  let dataValid = new Date(Date.UTC(dataErr[2],  dataErr[1], dataErr[0]));
+ let dataValid = new Date(Date.UTC(dataErr[2],  dataErr[1], dataErr[0]));
 //  console.log(dataValidErr)
 
-//  console.log(dataValid)
+ console.log(dataValid)
 
+let dat = setTimeTo6(new Date());
 
+console.log(dat)
 
+function setTimeTo6(updateDate) {
+    let date = new Date(updateDate);
+    let day = date.getUTCDate();
+    let month = date.getUTCMonth();
+    let year = date.getUTCFullYear();
+    
+    date.setHours(6,0,0,0);
+    date.setDate(day);
+    date.setMonth(month);
+    date.setYear(year);
+
+    return date;
+}
