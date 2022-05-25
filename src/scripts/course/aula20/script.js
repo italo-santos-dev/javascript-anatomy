463a55805850fb1e8d8a939b4c7bcb8cbd0b06c0
dev-italosantos <italotayllor@gmail.com>
2022-05-24 22:43:09 -0300

    feat: Add script to provide page refresh

--- src/scripts/course/aula20/script.js
+++ src/scripts/course/aula20/script.js
@@ -1,14 +1,10 @@
-function formattedDate(time){
-    if(time === undefined){
-        time = new Date();
-    }
-    time = new Date(time);
-    let h = time.getHours() < 10 ? '0'+ time.getHours() : time.getHours();
-    let m = time.getMinutes() < 10? '0'+time.getMinutes(): time.getMinutes();
-    
-    return time.toLocaleDateString('pt-BR') + ' '+h+':'+m;
-    
+function escopo() {
+    const form = document.querySelector('.formulario')
+
+    form.onsubmit = function (event) {
+        event.preventDefault();
+        console.log("Ainda é cedo")
+    };
 }
 
-
-console.log(formattedDate(new Date()));
+escopo();
