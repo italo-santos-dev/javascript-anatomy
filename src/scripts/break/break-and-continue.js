ecec6d8d1ef85bcc6d788d1dff7156848cf6f8b9
dev-italosantos <italotayllor@gmail.com>
2022-08-30 22:13:28 -0300

    feat: Add script to skip element of an array

---
+++ src/scripts/break/break-and-continue.js
@@ -0,0 +1,12 @@
+const numeros = [1, 2, 3, 4, 5.,6, 7, 8, 9];
+
+for (let i = 0; i < numeros.length; i++) {
+    let numero = numeros[i];
+
+    if (numero === 2) {
+        console.log("Test Continue");
+        continue;
+    }
+
+    console.log(numero);
+} 
