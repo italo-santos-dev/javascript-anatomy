c34858d100346fb3a90ea775b7fbf1b682bf3786
dev-italosantos <italotayllor@gmail.com>
2022-10-26 22:11:58 -0300

    refactor: comment the lines of console

--- src/scripts/date/add-working-days-date.js
+++ src/scripts/date/add-working-days-date.js
@@ -1,13 +1,13 @@
-let date = new Date("2022-07-24T21:39:01.004Z");
+let date = new Date("Wed Oct 26 2022 12:07:22 GMT-0300 (BRT)");
 date.setDate(date.getDate() + 3);
-console.log(date)
-console.log(new Date(new Date().setDate(new Date().getDate() + 1)))
+// console.log(date)
+//console.log(new Date(new Date().setDate(new Date().getDate() + 1)))
 
-console.log(date)
+// console.log(date)
 const dayOfWeek = date.getDay();
 if (dayOfWeek === 6) date.setDate(date.getDate() + 3);
 if (dayOfWeek === 0) date.setDate(date.getDate() + 2);
-console.log(data(date))
+console.log(data(new Date()))
 
 
 function data(date) {
@@ -30,7 +30,7 @@ function data(date) {
 let datas = new Date();
 let dataFinal = new Date(datas.getFullYear(), datas.getMonth(), datas.getDate(), 23, 59, 59);
 
-console.log(dataFinal)
+// console.log(dataFinal)
 
 
 let date1 = new Date();
@@ -39,9 +39,9 @@ let diffTime = Math.abs(date2 - date1);
 let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
 if (diffDays < 15) {
-    console.log("Test Gg")
+    // console.log("Test Gg")
 }
 
 var agora = new Date("2022-10-24T14:55:21.473Z");
 agora.toLocaleString("pt-br");
-console.log(agora);
+// console.log(agora);
