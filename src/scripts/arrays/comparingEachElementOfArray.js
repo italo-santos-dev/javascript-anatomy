96006e4f0a27982500f3455aeee71740dcc30c21
dev-italosantos <italotayllor@gmail.com>
2022-12-11 15:22:22 -0300

    feat: add folder of array

---
+++ src/scripts/arrays/comparingEachElementOfArray.js
@@ -0,0 +1,71 @@
+// comapring each element of array
+let _oldObject =  [
+    {
+       "pendingContentExtraction": false,
+       "name": "user.png",
+       "contentText": null,
+       "length": 1426,
+       "_id": "6390c937829a706fd2e53b30",
+       "contentType": "image/png",
+       "hash": "304918526ae81259a11031b0f8c761a0"
+    },
+    {
+        "pendingContentExtraction": false,
+        "name": "user.png",
+        "contentText": null,
+        "length": 1426,
+        "_id": "6390c937829a706fd2e53b30",
+        "contentType": "image/png",
+        "hash": "304918526ae81259a11031b0f8c761a0"
+     },
+     {
+      "pendingContentExtraction": false,
+      "name": "template.xlsx",
+      "contentText": null,
+      "length": 8972,
+      "_id": "626adc7312ac8920f98cdcd2",
+      "storage": "S3_AMAZON",
+      "collection": "uploadedFiles",
+      "contentType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "hash": "bc5276e013c082b7af547225f78101f6"
+   }
+ ]
+ 
+ let _object = [
+    {
+       "pendingContentExtraction": false,
+       "name": "user.png",
+       "contentText": null,
+       "length": 1426,
+       "_id": "6390c937829a706fd2e53b30",
+       "storage": "S3_AMAZON",
+       "collection": "uploadedFiles",
+       "contentType": "image/png",
+       "hash": "304918526ae81259a11031b0f8c761a0"
+    },
+    {
+       "pendingContentExtraction": false,
+       "name": "template.xlsx",
+       "contentText": null,
+       "length": 8972,
+       "_id": "626adc7312ac8920f98cdcd2",
+       "storage": "S3_AMAZON",
+       "collection": "uploadedFiles",
+       "contentType": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+       "hash": "bc5276e013c082b7af547225f78101f6"
+    }
+ ]
+
+const categoriasFiltradas = _object.filter(object => _oldObject.filter(oldObject => oldObject.hash === object.hash).length);
+console.log(categoriasFiltradas)
+//console.log(categoriasFiltradas.length > 1)
+//console.log(JSON.stringify(categoriasFiltradas));
+
+//   for(var i=0;i<a.length;i++) {
+//     if(a[i]!=b[i]) {
+//         console.log(false);
+//     } else {
+//         console.log(true);
+//     }
+//   }
+
