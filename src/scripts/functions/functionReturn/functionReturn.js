4fc3767470cf01bbd4940eb3317e96b20c5e92ae
dev-italosantos <italotayllor@gmail.com>
2022-12-12 21:55:20 -0300

    feat: add built-in functions

--- src/scripts/functions/functionReturn/functionReturn.js
+++ src/scripts/functions/functionReturn/functionReturn.js
@@ -8,3 +8,15 @@ const person_1 = createPerson('Italo', 'Santos');
 
 console.log(person_1);
 console.log(typeof person_1);
+
+function speakSentence(start) {
+    function talkRest(rest) {
+        return start + ' ' + rest;
+    }
+    return talkRest;
+}
+
+const speak = speakSentence("Hello");
+const rest = speak("World");
+
+console.log(rest);
