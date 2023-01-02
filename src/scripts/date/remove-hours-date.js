bef160d284042bb67fb45184e3b859fd4eef5e89
dev-italosantos <italotayllor@gmail.com>
2023-01-01 22:08:15 -0300

    feat: reset hours of dates

--- src/scripts/date/remove-hours-date.js
+++ src/scripts/date/remove-hours-date.js
@@ -1,4 +1,10 @@
 let data = new Date(new Date().setHours(-3, 0, 0, 0));
-let data2 = new Date(new Date("Wed Oct 26 2022 12:07:22 GMT-0300 (BRT)").setHours(-3, 0, 0, 0));
+let dataTest = new Date(new Date("Wed Oct 26 2022 12:07:22 GMT-0300 (BRT)").setHours(-3, 0, 0, 0));
 
-console.log(data2);
+
+let data1 = new Date();
+let data2 = new Date();
+let data1HoraZerada = new Date(data1.getFullYear(), data1.getMonth(), data1.getDate(), 0, 0, 0, 0);
+let data2HoraZerada = new Date(data2.getFullYear(), data2.getMonth(), data2.getDate(), 0, 0, 0, 0);
+
+console.log(dataTest);
