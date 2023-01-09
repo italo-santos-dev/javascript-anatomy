c1e0d8fc93548e7a1fba9268639cb620f6d6969e
dev-italosantos <italotayllor@gmail.com>
2023-01-09 08:59:21 -0300

    feat: generate random string

---
+++ src/scripts/strings/generate-random-string.js
@@ -0,0 +1,13 @@
+function getRandomString(length) {
+    let result = '';
+
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXZ' + 'abcdefghijklmnopqrstuvwxz0123456789';
+
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+console.log(getRandomString(10));
