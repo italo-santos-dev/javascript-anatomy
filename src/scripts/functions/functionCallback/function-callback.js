7d5bfe1b12abd0ed7032fc1c1bfceee841063b78
dev-italosantos <italotayllor@gmail.com>
2023-01-31 23:38:46 -0300

    feat: add functions for callback handling

--- src/scripts/functions/functionCallback/function-callback.js
+++ src/scripts/functions/functionCallback/function-callback.js
@@ -4,3 +4,25 @@ function rand(min = 1000, max = 3000) {
 }
 
 console.log(rand());
+
+function f1() {
+    setTimeout(function() {
+        console.log("f1");
+    }, rand());
+}
+
+function f2() {
+    setTimeout(function() {
+        console.log("f2");
+    }, rand());
+}
+
+function f3() {
+    setTimeout(function() {
+        console.log("f3");
+    }, rand());
+}
+
+f1();
+f2();
+f3();
