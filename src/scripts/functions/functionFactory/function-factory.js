9c19692eeda694b14049d796849f85178f68f56d
dev-italosantos <italotayllor@gmail.com>
2023-02-04 22:54:13 -0300

    feat: add function from imc to factory function create user

--- src/scripts/functions/functionFactory/function-factory.js
+++ src/scripts/functions/functionFactory/function-factory.js
@@ -1,12 +1,19 @@
- function createUser(name, lastname) {
+ function createUser(name, lastname, a, p) {
     return {
         name,
         lastname,
         speaks(subjectmatter) {
             return `${this.name} está ${subjectmatter}.`;
+        },
+        altura: a,
+        peso: p,
+        imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
         }
     }
  }
 
- const userID1 = createUser('Bianca',  'Cavalcante ');
+ const userID1 = createUser('Bianca',  'Cavalcante', 1.8, 80);
  console.log(userID1.speaks('loves to read books'));
+ console.log(userID1.imc());
