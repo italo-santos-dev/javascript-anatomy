61d3b80ec4864bd80ddbd26edcb3499099a024b0
dev-italosantos <italotayllor@gmail.com>
2023-12-17 18:45:19 -0300

    refactor: exercises course javascript class 35

--- src/views/course/aula35/assets/js/main.js
+++ src/views/course/aula35/assets/js/main.js
@@ -1,16 +1,16 @@
 const elements = [
-    {tag: 'p', texto: "Lorem lorem lorem"},
-    {tag: 'div', texto: "Lorem 1"},
-    {tag: 'section', texto: 'Lorem 2'},
-    {tag: 'footer', texto: 'Lorem 4'}
-]
+    { tag: 'p', texto: "Lorem lorem lorem" },
+    { tag: 'div', texto: "Lorem 1" },
+    { tag: 'section', texto: 'Lorem 2' },
+    { tag: 'footer', texto: 'Lorem 4' }
+];
 
 const container = document.querySelector('.container');
 const div = document.createElement('div');
 
 
 for (let i = 0; i < elements.length; i++) {
-    let {tag, texto} = elements[i];
+    let { tag, texto } = elements[i];
     let tagCreate = document.createElement(tag);
     tagCreate.innerHTML = texto;
     div.appendChild(tagCreate);
