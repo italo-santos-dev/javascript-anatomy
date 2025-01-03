886db6ac7c1fcaf373009fc04fad5660a498ad23
dev-italosantos <italotayllor@gmail.com>
2025-01-02 23:42:46 -0300

    feat: adiciona leitura e renderização de dados a partir de arquivo JSON

--- src/scripts/course/node/aula05/app.js
+++ src/scripts/course/node/aula05/app.js
@@ -1,13 +1,28 @@
 const path = require('path');
 const caminhoArquivo = path.resolve(__dirname, 'teste.json');
 const escreve = require('./modules/escrever');
+const ler = require('./modules/ler')
 
-const pessoas = [
-    { nome: 'Test 1'},
-    { nome: 'Test 2'},
-    { nome: 'Test 3'},
-    { nome: 'Test 4'},
-]
+// const pessoas = [
+//     { nome: 'Test 1'},
+//     { nome: 'Test 2'},
+//     { nome: 'Test 3'},
+//     { nome: 'Test 4'},
+// ]
 
-const json = JSON.stringify(pessoas, '', 2);
-escreve(caminhoArquivo, json)
+// const json = JSON.stringify(pessoas, '', 2);
+// escreve(caminhoArquivo, json)
+
+async function lerArquivo(caminho) {
+    const dados = await ler(caminho);
+    rederizaDados(dados);
+}
+
+function rederizaDados(dados) {
+    dados = JSON.parse(dados);
+    dados.forEach(element => {
+        console.log(element);
+    });
+}
+
+lerArquivo(caminhoArquivo);
