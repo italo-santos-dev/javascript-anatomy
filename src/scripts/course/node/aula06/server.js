26be0a908dcd3884254aba1cbfe9d639a3bb350a
dev-italosantos <italotayllor@gmail.com>
2025-01-07 22:56:09 -0300

    feat: adiciona rotas GET e POST ao servidor Express

--- src/scripts/course/node/aula06/server.js
+++ src/scripts/course/node/aula06/server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send(`
+        <from action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Enviar</button>
+        </from>  
+    `);
 });
+
+app.post('/', (req, res) => {
+    res.semd('Recebi o formulário');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entar em contato com a gente.');
+})
 
 app.listen(3000);
